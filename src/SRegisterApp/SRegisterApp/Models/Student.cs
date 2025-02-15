6916c3b463d9f51dded81d9c25f1e67047c384e6
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-02-15 11:27:09 -0400

    ready for the picture

---
+++ src/SRegisterApp/SRegisterApp/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SRegisterApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Matricula { get; set; }
+    }
+}
